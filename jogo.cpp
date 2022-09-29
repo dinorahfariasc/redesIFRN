7282ee8a2191977c1c16654c8f9eca9c16cb723f
dinorah <86006528+dinorahfariasc@users.noreply.github.com>
2022-09-29 10:26:33 -0300

    Add files via upload

---
+++ jogo.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+
+using namespace std; 
+
+int main () {
+	
+	int tabuleiro[9];
+	for (int i = 0; i< 9; i++){
+		tabuleiro[i = 0];		
+	} 
+	
+	cout << " " << tabuleiro[0] << " " << tabuleiro[1]<< " " << tabuleiro[2]<<endl
+	     << " " << tabuleiro[3] << " " << tabuleiro[4]<< " " << tabuleiro[5]<<endl
+	     << " " << tabuleiro[6] << " " << tabuleiro[7]<< " " << tabuleiro[8]<<endl
+	     <<endl;
+	
+	
+	return 0;
+	
+	 
+}
