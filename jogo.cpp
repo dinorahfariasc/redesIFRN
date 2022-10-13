93c02e4c0cffc32dca2edab9967d303139ff7ad4
dinorah <86006528+dinorahfariasc@users.noreply.github.com>
2022-10-13 10:21:14 -0300

    condicao de vitoria
    condicao de vitoria adicionada

--- jogo.cpp
+++ jogo.cpp
@@ -2,29 +2,29 @@
 using namespace std;
 int main()
 {
-    /*
+    
     char tabuleiro[9];
     for(int i = 0; i<9; i++){
         tabuleiro[i]= '.';
     }
-   */ 
-    string tabuleiro[3][3] = 
-                    {{".", ".","."},
-                    {".", ".", "."},
-                    {".", ".", "."}};
-                    
-    //constexpr int vit[3] = {(0,4,8),(0,1,2),(0,3,6)};
    
-    int pos1;
-    int pos2;
+   for(int i = 0; i<9; i++){
+        tabuleiro[i] != '.';
+        
+    }
+   
+   
+    int pos;
     cout << tabuleiro<< endl; 
+    
     for(int i = 2; i >0; i++){
         
+     
+       
         // jogador 1
        if (i%2 == 0){
         cout << "jogador1 digite uma posicao (0-8)" << endl;
         cin >> pos;
-        
         if (tabuleiro[pos] == '.'){
            tabuleiro[pos] = 'X';
            cout << tabuleiro << endl;
@@ -36,12 +36,7 @@ int main()
            cin >> pos;
            tabuleiro[pos] = 'X';
            cout << tabuleiro << endl;
-           
         }
-        
-        
-        
-          
        }
        
        // jogador 2
@@ -65,35 +60,132 @@ int main()
        
        }
        
-   
-   
-}
-
-
-   
+       
+       
+       }
+       
         
-    /*  condicao de vitoria 
-        se os mesmos caracteres estiverem em uma determinada sequencia, há vitoria 
         
-        if (tabuleiro[0,4,8] == 'X'){
-           cout << "vitoria jogador1" << endl;
-           break;
+        
+        
+    // condicao vitoria ===========================================================================
+       
+       int empate = 0;
+       // linha hori 0
+       if(tabuleiro[0] != '.'){
+           if (tabuleiro[0] == 'X' && tabuleiro[1] == 'X' && tabuleiro[2] == 'X'){
+               vitoria;
+               
+           }
+           else if (tabuleiro[0] == 'O' && tabuleiro[1] == 'O' && tabuleiro[2] == 'O'){
+               cout << "vitória do jogador2 na linha 1" << endl;
+               
+           }
+       }
+       
+       // linha hori 1
+       else if(tabuleiro[3] != '.'){
+           if (tabuleiro[3] == 'X' && tabuleiro[4] == 'X' && tabuleiro[5] == 'X'){
+               cout << "vitoria do jogador1 na linha 2" << endl;
+               
+           }
+           else if (tabuleiro[3] == 'O' && tabuleiro[4] == 'O' && tabuleiro[5] == 'O'){
+               cout << "vitória do jogador2 na linha 2" << endl;
+               
+           }
+           else {
+               empate = empate + 1; 
+           }
+       
+           
+   
+        }
+
+        // linha hori 2
+        else if(tabuleiro[6] != '.'){
+           if (tabuleiro[6] == 'X' && tabuleiro[7] == 'X' && tabuleiro[8] == 'X'){
+               cout << "vitoria do jogador1 na linha 1" << endl;
+               
+           }
+           else if (tabuleiro[6] == 'O' && tabuleiro[7] == 'O' && tabuleiro[8] == 'O'){
+               cout << "vitória do jogador2 na linha 1" << endl;
+               
+           }
            
        }
        
-       */
-  
-    /* if ((tabuleiro[0,4,8] == "X") || (tabuleiro[1,4,7] == "X") ||(tabuleiro[0,1,2] == "X") ||(tabuleiro[0,3,6] == "X") ||(tabuleiro[2,4,6] == "X") ||(tabuleiro[2,5,8] == "X") ||(tabuleiro[3,4,5] == "X") ||(tabuleiro[6,7,8] == "X")) {
-        
-        cout << "jogador1 ganhou!" << endl;
-    }    
-    
-    
-    if (tabuleiro[0] == 'X' && [4][8] == {"X"}){
-            cout << "jogador1 ganhou !" << endl;
-            i = 0;
-        }
-    */
+        // linha vertical 1 
+        else if(tabuleiro[0] != '.'){
+           if (tabuleiro[0] == 'X' && tabuleiro[3] == 'X' && tabuleiro[6] == 'X'){
+               cout << "vitoria do jogador1 na linha 1" << endl;
+               
+           }
+           else if (tabuleiro[0] == 'O' && tabuleiro[3] == 'O' && tabuleiro[6] == 'O'){
+               cout << "vitória do jogador2 na linha 1" << endl;
+               
+           }
+           
+       }
+   
+         // linha vertical 2
+        else if(tabuleiro[1] != '.'){
+           if (tabuleiro[1] == 'X' && tabuleiro[4] == 'X' && tabuleiro[7] == 'X'){
+               cout << "vitoria do jogador1 na linha 1" << endl;
+               
+           }
+           else if (tabuleiro[1] == 'O' && tabuleiro[4] == 'O' && tabuleiro[7] == 'O'){
+               cout << "vitória do jogador2 na linha 1" << endl;
+               
+           }
+           
+       }
+       
+         // linha vertical 3 
+        else if(tabuleiro[2] != '.'){
+           if (tabuleiro[2] == 'X' && tabuleiro[5] == 'X' && tabuleiro[8] == 'X'){
+               cout << "vitoria do jogador1 na linha 1" << endl;
+               
+           }
+           else if (tabuleiro[2] == 'O' && tabuleiro[5] == 'O' && tabuleiro[8] == 'O'){
+               cout << "vitória do jogador2 na linha 1" << endl;
+               
+           }
+           
+       }
+       
+        // diagonal 1
+        else if(tabuleiro[0] != '.'){
+           if (tabuleiro[0] == 'X' && tabuleiro[4] == 'X' && tabuleiro[8] == 'X'){
+               cout << "vitoria do jogador1 na linha 1" << endl;
+               
+           }
+           else if (tabuleiro[0] == 'O' && tabuleiro[4] == 'O' && tabuleiro[8] == 'O'){
+               cout << "vitória do jogador2 na linha 1" << endl;
+               
+           }
+           
+       }
+       
+       // diagonal 2
+        else if(tabuleiro[2] != '.'){
+           if (tabuleiro[2] == 'X' && tabuleiro[4] == 'X' && tabuleiro[6] == 'X'){
+               cout << "vitoria do jogador1 na linha 1" << endl;
+               
+           }
+           else if (tabuleiro[2] == 'O' && tabuleiro[4] == 'O' && tabuleiro[6] == 'O'){
+               cout << "vitória do jogador2 na linha 1" << endl;
+               
+           }
+           
+       }
+       
+       //empate 
+       else {
+           
+           cout << "empate"
+       }
+   
+        // ==================================================================================
     
     
     
@@ -101,3 +193,4 @@ int main()
 
   return 0;
 }
+   
